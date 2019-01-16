ead3c68fb17a3796bb1784d05b7ee94629fd2c04
YogSottot <7411302+YogSottot@users.noreply.github.com>
2019-01-16 17:38:14 +0300

    Add terraform hw without stars

---
+++ terraform/lb.tf
@@ -0,0 +1,53 @@
+resource "google_compute_instance" "lb" {
+  name         = "reddit-lb"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-lb"]
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+}
+
+resource "google_compute_firewall" "firewall_lb" {
+  name = "allow-lb-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-lb"]
+}
